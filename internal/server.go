aac7558771ded981a5a59d632dbeba59f151f44a
AniMar0 <mr.zakariakahlaoui@gmail.com>
2026-02-27 01:11:42 +0000

    Refactor Server struct and methods for improved clarity and organization

--- internal/server.go
+++ internal/server.go
@@ -3,12 +3,34 @@ package internal
 import (
 	"database/sql"
 	"net/http"
+	Rp "url-shortener/internal/repository"
 )
 
 type Server struct {
-	Host         string         `json:"host"`
-	Port         string         `json:"port"`
-	DatabasePath string         `json:"database_path"`
-	Database     *sql.DB        `json:"-"`
-	Router       *http.ServeMux `json:"-"`
+	Host          string         `json:"host"`
+	Port          string         `json:"port"`
+	DatabasePath  string         `json:"database_path"`
+	Database      *sql.DB        `json:"-"`
+	Router        *http.ServeMux `json:"-"`
+}
+
+func NewServer(host, port, databasePath string) *Server {
+	return &Server{
+		Host:         host,
+		Port:         port,
+		DatabasePath: databasePath,
+	}
+}
+
+func (s *Server) Start() error {
+	s.Database = Rp.InitDB(s.DatabasePath)
+	s.Router = http.NewServeMux()
+	s.routes()
+	return http.ListenAndServe(s.Host+":"+s.Port, s.Router)
+}
+
+func (s *Server) routes() {
+	//s.Router.HandleFunc("/api/register", s.handleRegister)
+	//s.Router.HandleFunc("/api/login", s.handleLogin)
+	//s.Router.HandleFunc("/", s.handleRedirect)
 }
